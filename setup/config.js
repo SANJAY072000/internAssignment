a28a3d80ad8af2f1ed30999b4d2dad5885c30008
SANJAY SINGH BISHT <39243846+SANJAY072000@users.noreply.github.com>
2021-05-09 14:47:33 +0530

    updated code

--- setup/config.js
+++ setup/config.js
@@ -1,5 +1,4 @@
 // exporting the configurations
 module.exports={
-    mongoUrl:'mongodb+srv://sanjay_20:san07jay@cluster0-yxg19.mongodb.net/test?retryWrites=true&w=majority',
-    secret:'Work Hard'
+    
 };
